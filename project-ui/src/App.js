f27d70a1c242763b71084b994739e56b0de72614
yuan <2651123317@qq.com>
2022-04-27 18:46:50 -0400

    login api 2

--- project-ui/src/App.js
+++ project-ui/src/App.js
@@ -1,18 +1,40 @@
 import logo from "./pics/NYU_Stacked_RGB_Color.png";
 import "./App.css";
 import { LockClosedIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 function App() {
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.target);
+
+		fetch("http://localhost:8080/api/user/login", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				userId: formData.get("userId"),
+				pwd: formData.get("password"),
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+			.catch((err) => err);
+	};
+
 	return (
 		<>
 			{/*
-		This example requires updating your template:
+        This example requires updating your template:
 
-		```
-		<html class="h-full bg-gray-50">
-		<body class="h-full">
-		```
-		*/}
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+        */}
 			<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 				<div className="max-w-md w-full space-y-8">
 					<div>
@@ -29,21 +51,22 @@ function App() {
 							</a>
 						</p>
 					</div>
-					<form className="mt-8 space-y-6" action="#" method="POST">
+					<form className="mt-8 space-y-6" onSubmit={onSubmit} method="POST">
 						<input type="hidden" name="remember" defaultValue="true" />
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div>
-								<label htmlFor="email-address" className="sr-only">
-									Email address
+								<label htmlFor="userId-address" className="sr-only">
+									userId address
 								</label>
 								<input
-									id="email-address"
-									name="email"
-									type="email"
-									autoComplete="email"
+									id="userId-address"
+									name="userId"
+									type="userId"
+									autoComplete="userId"
 									required
 									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
-									placeholder="Email address"
+									placeholder="userId address"
+									// onFocus={onUserIdFocus}
 								/>
 							</div>
 							<div>
@@ -58,6 +81,7 @@ function App() {
 									required
 									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
 									placeholder="Password"
+									// onFocus={onPwdFocus}
 								/>
 							</div>
 						</div>
@@ -70,6 +94,7 @@ function App() {
 									type="checkbox"
 									className="h-4 w-4 text-purple-700 focus:ring-purple-500 border-gray-300 rounded"
 								/>
+
 								<label
 									htmlFor="remember-me"
 									className="ml-2 block text-sm text-gray-900"
